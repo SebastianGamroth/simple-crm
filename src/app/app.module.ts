bfb0d940e8809017f3bd6727de1ad7dc4960c264
user7d11 <user7w11@gmail.com>
2022-08-05 23:35:30 +0200

    UserDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,13 +27,15 @@ import { environment } from 'src/environments/environment';
 // import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatCardModule} from '@angular/material/card';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     UserComponent,
-    DialogAddUserComponent
+    DialogAddUserComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
