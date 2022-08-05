f91ffdcf3b9acfd68c7091d66078d8ce1580ce59
user7d11 <user7w11@gmail.com>
2022-08-05 13:13:28 +0200

    firebase add new project and link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-5516c',
+    appId: '1:577625992150:web:deea2fd24e0db39b539277',
+    storageBucket: 'simple-crm-5516c.appspot.com',
+    apiKey: 'AIzaSyBfLl-H_sPA2721G0g412thzr7bO7sXYoU',
+    authDomain: 'simple-crm-5516c.firebaseapp.com',
+    messagingSenderId: '577625992150',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-5516c',
+    appId: '1:577625992150:web:deea2fd24e0db39b539277',
+    storageBucket: 'simple-crm-5516c.appspot.com',
+    apiKey: 'AIzaSyBfLl-H_sPA2721G0g412thzr7bO7sXYoU',
+    authDomain: 'simple-crm-5516c.firebaseapp.com',
+    messagingSenderId: '577625992150',
+  },
   production: false
 };
 
