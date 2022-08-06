c32adbdf9f72f3ddc45e8d8845d374548f85b9bc
user7d11 <user7w11@gmail.com>
2022-08-06 14:13:36 +0200

    Get user with ID from Firebase DB

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/users.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,13 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetailComponent implements OnInit {
   userId = '';
+  user: User = new User();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.paramMap.get('id');
     console.log(this.userId)
+    this.getUser();
+  }
 
+  getUser() {
+    this.firestore.collection('users').doc(this.userId).valueChanges().subscribe((user: any) => {
+      this.user = new User(user);
+      console.log(this.user)
+    })
   }
 
 }
